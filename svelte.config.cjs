41e58a44fad14fad78bb030f6d92681f4201100f
Robert Schäfer <git@roschaefer.de>
2021-05-03 16:27:25 +0200

    feat: setup svelte with tailwind

---
+++ svelte.config.cjs
@@ -0,0 +1,21 @@
+const preprocess = require('svelte-preprocess');
+const static = require('@sveltejs/adapter-static');
+
+/** @type {import('@sveltejs/kit').Config} */
+module.exports = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		}),
+	],
+
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: {
+			adapt: static
+		}
+	}
+};
