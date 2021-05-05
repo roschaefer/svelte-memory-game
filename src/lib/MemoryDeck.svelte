6ed25526b44fd1f51eb90eb6f8af1d5a55e2bbc5
Robert Schäfer <git@roschaefer.de>
2021-05-06 01:39:33 +0200

    refactor

--- src/lib/MemoryDeck.svelte
+++ src/lib/MemoryDeck.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { CardState } from '$lib/cardStates.ts'
 	import MemoryCard  from '$lib/MemoryCard.svelte';
+	const { hidden, revealed, flipped } = CardState
 	/**
 	 * Shuffles array in place. ES6 version
 	 * @param {Array} a items An array containing the items.
@@ -23,28 +24,27 @@
 		'bg-indigo-800'
 	];
 
-	let motifs = colors.concat(colors).map((color, index) => ({ id: index, color, state: CardState.hidden })) ;
+	let motifs = colors.concat(colors).map((color, index) => ({ color, state: hidden })) ;
 
-	function handleClick (index) {
-		const flipped = motifs.filter(motif => motif.state === CardState.flipped)
-		if (flipped.length >= 2) {
-			const [one, other] = flipped
+	function handleClick (toFlip) {
+		const flippedMotifs = motifs.filter(motif => motif.state === flipped)
+		if (flippedMotifs.length >= 2) {
+			const [one, other] = flippedMotifs
 			if(one.color === other.color) {
-				one.state = CardState.revealed
-				other.state = CardState.revealed
+				one.state = revealed
+				other.state = revealed
 			} else {
-				one.state = CardState.hidden
-				other.state = CardState.hidden
+				one.state = hidden
+				other.state = hidden
 			}
 		}
-		const toFlip = motifs.find(motif => motif.id === index)
-		toFlip.state = CardState.flipped
+		toFlip.state = flipped
 		motifs = motifs
 	};
 </script>
 
 <div class="grid grid-cols-4 gap-4 h-96 w-96">
-	{#each motifs as motif (motif.id)}
-		<MemoryCard motif={motif} on:click={handleClick(motif.id)} />
+	{#each motifs as motif, i (i)}
+		<MemoryCard motif={motif} on:click={handleClick(motif)} />
 	{/each}
 </div>
